65ada10dd0871ff8dd516fd00b2b4c0b5523bc61
Jacob Burenstam <burenstam@gmail.com>
2013-12-08 17:37:17 +0100

    Updated import script. Added git commit shorthand

--- setup/import.sh
+++ setup/import.sh
@@ -1,6 +1,11 @@
-source ~/.dot-bash/unix-profile
-if [ "$(uname)" == "Darwin" ]; then
-    source ~/.dot-bash/osx-profile
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    source ~/.dot-bash/linux-profile
-fi
+# dot-bash import
+function source_dot_bash_files {
+  source ~/.dot-bash/unix-profile
+  if [ "$(uname)" == "Darwin" ]; then
+      source ~/.dot-bash/osx-profile
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+      source ~/.dot-bash/linux-profile
+  fi
+}
+source_dot_bash_files
+# END dot-bash import
