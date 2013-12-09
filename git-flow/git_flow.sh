26a481f1de40231898ab57c8ca9a90b8e49d72f6
Jacob Burenstam <burenstam@gmail.com>
2013-12-10 00:40:45 +0100

    update

--- git-flow/git_flow.sh
+++ git-flow/git_flow.sh
@@ -24,9 +24,9 @@ function pvg-update {
   git pull origin master
 }
 
-function pvg-dev-done-help {
+function pvg-done-help {
   echo -e "usage: "
-  echo -e "\t pvg-dev-done 'message'"
+  echo -e "\t pvg-done 'message'"
   echo -e "\t message is your commit message"
   echo -e ""
   echo -e "example: "
@@ -36,14 +36,15 @@ function pvg-dev-done-help {
   echo -e "\t etc....."
 }
 
-function pvg-dev-done {
-  git commit -am $1
+function pvg-done {
+  git add --all
+  git commit -m $1
   local BRANCH="$(git rev-parse --abbrev-ref HEAD)"
   git push origin $BRANCH
   git checkout master
   git pull origin master
   git merge $BRANCH
-  echo "Fix your (potential) conflicts and then run:"
+  echo -e "When ready run: "
   echo -e "\t pvg-ready-to-release"
 }
 
@@ -61,7 +62,7 @@ function pvg-ready-to-release {
   else
     echo -e "Cannot release from branch $($CURRENT_BRANCH)."
     echo -e "Can only release from master branch."
-    echo -e "Did you forget to run pvg-dev-done?"
+    echo -e "Did you forget to run pvg-done?"
   fi
 }
 
@@ -70,6 +71,6 @@ function pvg-flow-help {
   echo -e "Run any command followed by -help for command details."
   echo -e "example: "
   echo -e "\t $ pvg-dev story23"
-  echo -e "\t $ pvg-dev-done 'Implemented Story 23'"
+  echo -e "\t $ pvg-done 'Implemented Story 23'"
   echo -e "\t $ pvg-ready-to-release"
 }
