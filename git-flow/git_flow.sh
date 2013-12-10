b9c51b39e9c487e065587ac44aba1ef2b7f39537
Jacob Burenstam <burenstam@gmail.com>
2013-12-10 12:33:29 +0100

    Re-arragnged functions

--- git-flow/git_flow.sh
+++ git-flow/git_flow.sh
@@ -1,12 +1,3 @@
-function pvg-dev-help {
-  echo -e "description: "
-  echo -e "\t start working on your feature."
-  echo -e "usage: "
-  echo -e "\t pvg-dev a_branch_name"
-  echo ""
-  echo -e "\t guarantees clean working set based of master"
-}
-
 function pvg-dev {
   git checkout master
   git checkout -b $1
@@ -14,26 +5,8 @@ function pvg-dev {
   echo -e "You're now ready to develop your story."
 }
 
-function pvg-update_help {
-  echo -e "usage: "
-  echo -e "\t pvg-update"
-  echo -e "Syncs with repository."
-}
-
 function pvg-update {
   git pull origin master
-}
-
-function pvg-done-help {
-  echo -e "usage: "
-  echo -e "\t pvg-done 'message'"
-  echo -e "\t message is your commit message"
-  echo -e ""
-  echo -e "example: "
-  echo -e "\t Implemented story 13."
-  echo -e "\t Updated FileReaderInterface."
-  echo -e "\t Fixed merge conflict."
-  echo -e "\t etc....."
 }
 
 function pvg-done {
@@ -48,12 +21,6 @@ function pvg-done {
   echo -e "\t pvg-ready-to-release"
 }
 
-function pvg-ready-to-release-help {
-  echo -e "usage: "
-  echo -e "\t pvg-ready-to-release"
-  echo -e "\t pushes your comitted changed to the repository."
-}
-
 function pvg-ready-to-release {
   local BRANCH=$(git rev-parse --abbrev-ref HEAD)
   if [ $BRANCH == "master" ]; then
@@ -66,6 +33,10 @@ function pvg-ready-to-release {
   fi
 }
 
+################
+#     HELP     #
+################
+
 function pvg-flow-help {
   echo -e "PVG-FLOW"
   echo -e "Run any command followed by -help for command details."
@@ -73,4 +44,37 @@ function pvg-flow-help {
   echo -e "\t $ pvg-dev story23"
   echo -e "\t $ pvg-done 'Implemented Story 23'"
   echo -e "\t $ pvg-ready-to-release"
+}
+
+function pvg-dev-help {
+  echo -e "description: "
+  echo -e "\t start working on your feature."
+  echo -e "usage: "
+  echo -e "\t pvg-dev a_branch_name"
+  echo ""
+  echo -e "\t guarantees clean working set based of master"
+}
+
+function pvg-update_help {
+  echo -e "usage: "
+  echo -e "\t pvg-update"
+  echo -e "Syncs with repository."
+}
+
+function pvg-done-help {
+  echo -e "usage: "
+  echo -e "\t pvg-done 'message'"
+  echo -e "\t message is your commit message"
+  echo -e ""
+  echo -e "example: "
+  echo -e "\t Implemented story 13."
+  echo -e "\t Updated FileReaderInterface."
+  echo -e "\t Fixed merge conflict."
+  echo -e "\t etc....."
+}
+
+function pvg-ready-to-release-help {
+  echo -e "usage: "
+  echo -e "\t pvg-ready-to-release"
+  echo -e "\t pushes your comitted changed to the repository."
 }
