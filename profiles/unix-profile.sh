56b935500f8573c96d86846a539fe2e03c5d688c
Jacob Burenstam <burenstam@gmail.com>
2013-12-24 19:43:08 +0100

    alias j='jump'

--- profiles/unix-profile.sh
+++ profiles/unix-profile.sh
@@ -90,17 +90,18 @@ alias ......='cd ../../../../../'
 #         http://jeroenjanssens.com/2013/08/16/quickly-navigate-your-filesystem-from-the-command-line.html
 export MARKPATH=$HOME/.marks
 function jump {
-    cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
+  cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
 }
 function mark {
-    mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1"
+  mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1"
 }
 function unmark {
-    rm -i "$MARKPATH/$1"
+  rm -i "$MARKPATH/$1"
 }
 function marks {
-    ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
+  ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
 }
+alias j="jump"
 
 
 # Highlights and prints the lines numbers of all non US ASCII characters
