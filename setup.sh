0c1419015cec2c39a73ea35d91b08fece7f4c88f
Jacob Burenstam <burenstam@gmail.com>
2014-01-11 23:42:10 +0100

    Fix.

--- setup.sh
+++ setup.sh
@@ -2,12 +2,15 @@ function __dot-bash-inject-to-profile {
   if [[ -f ~/.bash_profile ]]; then
     echo "Injecting import: bash_profile"
     cat ~/.buren/dot-bash/setup/import/import_to_bash_profile >> ~/.bash_profile
+    source ~/.bash_profile
   elif [[ -f ~/.bashrc ]]; then
     echo "Injecting import: bashrc"
     cat ~/.buren/dot-bash/setup/import/import_to_bash_profile >> ~/.bashrc
+    source ~/.bashrc
   elif [[ -f ~/.zshrc ]]; then
     echo "Injecting import: zshrc"
     cat ~/.buren/dot-bash/setup/import/import_to_bash_profile >> ~/.zshrc
+    source ~/.zshrc
   else
     echo "[ERROR] Neither .bash_profile, .bashrc or .zshrc found."
     echo "However the script has been loaded and will be available in the current shell session."
@@ -15,7 +18,6 @@ function __dot-bash-inject-to-profile {
     echo -e "To install add the below line to your bash profile."
     echo -e "\t source ~/.git-story/setup/import.sh"
   fi
-  source ~/.buren/dot-bash/setup/import/import_to_bash_profile
 }
 
 mkdir ~/.buren
