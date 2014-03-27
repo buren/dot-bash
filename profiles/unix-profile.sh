6fa4f2315d34c5131839efdc6e96fde572d15bb7
Jacob Burenstam <burenstam@gmail.com>
2014-03-27 18:27:08 +0100

    update

--- profiles/unix-profile.sh
+++ profiles/unix-profile.sh
@@ -9,10 +9,21 @@ if [[ $SHELL != '/bin/zsh' ]]; then
   shopt -s cdspell
 fi
 
+
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+	colorflag="--color"
+else # OS X `ls`
+	colorflag="-G"
+fi
+
+
 # Highlight section titles in manual pages
 export LESS_TERMCAP_md="${ORANGE}"
 # Always enable colored `grep` output
 export GREP_OPTIONS="--color=auto"
+export GREP_COLOR='1;31' # green for matches
+
 
 # Larger bash history (allow 32³ entries; default is 500)
 export HISTSIZE=32768
@@ -20,6 +31,12 @@ export HISTFILESIZE=${HISTSIZE}
 export HISTCONTROL=ignoredups
 # Make some commands not show up in history
 export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
+
+# Highlight section titles in manual pages
+export LESS_TERMCAP_md="$ORANGE"
+
+# Don’t clear the screen after quitting a manual page
+export MANPAGER="less -X"
 
 if [[ -f ~/.inputrc ]]; then
   completion_inserted=$(cat $HOME/.inputrc | grep "completion-ignore-case")
@@ -32,6 +49,11 @@ else
   echo "No inputrc file found. Creating..."
   cat $HOME/.buren/dot-bash/.inputrc > $HOME/.inputrc
   echo "Inserted completion-ignore-case to ~/.inputrc..."
+fi
+
+if [[ ! -f ~/.curlrc ]];then
+  echo "No curlrc found, injecting..."
+  cat $HOME/.buren/dot-bash/.curlrc > $HOME/.curlrc
 fi
 
 ## __DOT_BASH__ ##
@@ -108,7 +130,20 @@ count_files() {
   ls $1 | wc -l
 }
 alias lc='count_files' # short hand for the above function
-alias l='ls -lAh --color'
+
+
+# List all files colorized in long format
+alias l="ls -lF ${colorflag}"
+
+# List all files colorized in long format, including dot files
+alias la="ls -laF ${colorflag}"
+
+# List only directories
+alias lsd="ls -lF ${colorflag} | grep --color=never '^d'"
+
+# Always use color output for `ls`
+alias ls="command ls ${colorflag}"
+export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
 
 alias hs='history | grep --color=auto'
 alias o='open'
@@ -118,6 +153,9 @@ alias mv='mv -i' # prompt if duplicate filename
 alias tree='tree -C $* | less -R'
 # Search running processes
 alias tm='ps -ef | grep --color=auto'
+
+# Enable aliases to be sudo’ed
+alias sudo='sudo '
 
 # Do sudo a command, or do sudo to the last typed command if no argument given
 please() {
@@ -272,6 +310,11 @@ lein() {
 }
 
 ## __NETWORKING__ ##
+
+# One of @janmoesen’s ProTip™s
+for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
+	alias "$method"="lwp-request -m '$method'"
+done
 
 # Create a data URL from a file
 dataurl() {
@@ -645,3 +688,11 @@ function calc() {
   fi
   printf "\n"
 }
+
+# Stopwatch
+alias timer='echo "Timer started. Stop with Ctrl-D." && date && time cat && date'
+
+# JavaScriptCore REPL
+jscbin="/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc"
+[ -e "${jscbin}" ] && alias jrb="${jscbin}"
+unset jscbin
