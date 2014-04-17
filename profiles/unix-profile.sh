80e4c42592e7737fc25d3202d75590421a26c6ce
Jacob Burenstam <burenstam@gmail.com>
2014-04-17 10:38:19 +0200

    xbmc_restart command

--- profiles/unix-profile.sh
+++ profiles/unix-profile.sh
@@ -98,6 +98,7 @@ ssh-remember-me() {
 if [[ $B_HAS_RASPBERRY == true ]]; then
   alias xbmc_start='sudo initctl start xbmc'
   alias xbmc_stop='sudo initctl stop xbmc'
+  alias xbmc_restart='xbmc_stop && xbmc_start'
 
   pi_login() {
     ssh $B_PI_USERNAME@$B_PI_LOGIN "$@"
